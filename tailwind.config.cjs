8980723b2fca11fdd678fa01ca41a55dcf315f48
Douglas Ward <douglas@giantmonkey.de>
2022-08-27 13:22:32 +0200

    setup tooling

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+const typography = require("@tailwindcss/typography");
+const animationDelay = require("tailwindcss-animation-delay");
+
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+
+  theme: {
+    fontFamily: {
+      sans: "Inter",
+    },
+    extend: {
+      colors: {
+        brand: "#11a683",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "scale(1) translateY(0px)" },
+          "50%": { transform: "scale(1.2) translateY(-50px) rotate(45deg)" },
+        },
+      },
+      animation: {
+        float: "float 20s linear infinite",
+      },
+    },
+  },
+
+  plugins: [typography, animationDelay],
+};
+
+module.exports = config;
