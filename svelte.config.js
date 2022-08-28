565820236c07a5305da40e76e8ef1c60a0d7bd09
Douglas Ward <douglas@giantmonkey.de>
2022-08-28 12:09:05 +0200

    don't use tabs

--- svelte.config.js
+++ svelte.config.js
@@ -3,18 +3,18 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true
-	}),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    postcss: true
+  }),
 
-	kit: {
-		adapter: adapter({ pages: 'build', assets: 'build', fallback: null, precompress: false }),
-		prerender: {
-			default: true
-		}
-	}
+  kit: {
+    adapter: adapter({ pages: 'build', assets: 'build', fallback: null, precompress: false }),
+    prerender: {
+      default: true
+    }
+  }
 };
 
 export default config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,29 +2,29 @@ const typography = require('@tailwindcss/typography');
 const animationDelay = require('tailwindcss-animation-delay');
 
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		fontFamily: {
-			sans: 'Inter'
-		},
-		extend: {
-			colors: {
-				brand: '#11a683'
-			},
-			keyframes: {
-				float: {
-					'0%, 100%': { transform: 'scale(1) translateY(0px)' },
-					'50%': { transform: 'scale(1.2) translateY(-50px) rotate(45deg)' }
-				}
-			},
-			animation: {
-				float: 'float 20s linear infinite'
-			}
-		}
-	},
+  theme: {
+    fontFamily: {
+      sans: 'Inter'
+    },
+    extend: {
+      colors: {
+        brand: '#11a683'
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'scale(1) translateY(0px)' },
+          '50%': { transform: 'scale(1.2) translateY(-50px) rotate(45deg)' }
+        }
+      },
+      animation: {
+        float: 'float 20s linear infinite'
+      }
+    }
+  },
 
-	plugins: [typography, animationDelay]
+  plugins: [typography, animationDelay]
 };
 
 module.exports = config;
