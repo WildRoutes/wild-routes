a72d376a54df89f33467e11d5dd524c0717c5a32
Douglas Ward <douglas@giantmonkey.de>
2022-08-28 11:02:58 +0200

    add hero header to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="max-h-screen overflow-hidden relative flex items-center content-center">
+    <img class="w-full h-full align-middle" src="/images/home.png">
+    <div class="text-center absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center text-white">
+        <h1 class="mb-5 text-5xl font-bold">
+            Wild Routes
+        </h1>
+        <p class="italic">
+            Reclaim your education... Equip yourself for life... on your own terms.
+        </p>
+    </div>
+</header>
+!
