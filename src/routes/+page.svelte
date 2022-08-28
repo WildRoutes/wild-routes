d2c239a015db30efa0985f7b38bf4ca799704626
Douglas Ward <douglas@giantmonkey.de>
2022-08-28 12:08:50 +0200

    fix header height on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <header class="max-h-screen overflow-hidden relative flex items-center content-center">
-    <img class="w-full h-full align-middle" src="/images/home.png">
+    <img class="sm:min-w-full h-screen max-w-none align-middle" src="/images/home.png">
     <div class="text-center absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center text-white">
         <h1 class="mb-5 text-5xl font-bold">
             Wild Routes
@@ -9,4 +9,3 @@
         </p>
     </div>
 </header>
-!
